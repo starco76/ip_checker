70f10b5ff6468a365194cdc49aa74932bd85a857
Mojtaba Tahmasbi <89037975+starco76@users.noreply.github.com>
2024-06-22 11:24:41 +0330

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ async def read_root(data:data):
 if __name__ == "__main__":
     uvicorn.run(app,host='0.0.0.0',port=5050)
 
-#@reboot nohup /root/ipchecker/.env/bin/python /root/ipchecker/main.py &
+#@reboot nohup /root/ip_checker/.env/bin/python /root/ip_checker/main.py &
